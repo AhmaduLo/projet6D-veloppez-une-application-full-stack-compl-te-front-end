4475f01dd2a120c17b88ae73130e2e66290e0fa6
AhmaduLo <ahmadubambagayelo@gmail.com>
2025-06-18 15:49:04 +0200

    regle conflit

--- src/app/pages/article-detail/article-detail.component.ts
+++ src/app/pages/article-detail/article-detail.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-detail',
-  template: `
-    <app-commentaire></app-commentaire>
-  `,
-  styles: [
-  ]
+  template: `<app-commentaire></app-commentaire>`,
+  styleUrls: ['./article-detail.component.scss']
 })
 export class ArticleDetailComponent implements OnInit {
 
